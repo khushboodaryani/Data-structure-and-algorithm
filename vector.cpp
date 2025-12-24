73ffce69c2ffbad9bab47edf6ccb5d3c3810904d
khushboodaryani <khushboodaryani1@gmail.com>
2025-12-24 20:46:46 +0530

    updated

---
+++ vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// vector are dynamic we don't need to specify the size
+int main(){
+    vector<int>myVec;
+    myVec = {1,2,3,4,5};
+    for (int i :myVec){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
